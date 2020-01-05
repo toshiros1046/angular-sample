1a39731f6ef2bb5dc942ece061958b112d0cc070
佐藤 駿郎 <toshiros1046@gmail.com>
2020-01-06 01:07:23 +0900

    20200105chartdone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,85 @@
-import { Component } from '@angular/core';
-
-// ① 型定義
-declare function Hello(): void;
-// 型定義はこれ↓でも良い
-// declare var Hello: any
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-sample';
+export class AppComponent implements OnInit{
+  context01: CanvasRenderingContext2D;
 
-  // ② hello.jsのHello()を呼び出し
-  constructor() {
-  Hello();
+  @ViewChild('mycanvas01',{static:true}) mycanvas01: ElementRef;
+
+  ngOnInit() {
+    this.context01 = this.mycanvas01.nativeElement.getContext('2d');
+    let mycanvas01 = new Chart(this.context01, {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: 'Data01',
+          backgroundColor: 'rgba(255, 255, 153, 0.5)',
+          borderColor: "rgba(255, 99, 132, 0)",
+          pointRadius: 0,
+          fill: true,
+          data: [
+            { x: 0, y: 2 },
+            { x: 1, y: 1 },
+            { x: 2, y: 2.5 },
+            { x: 3, y: 5 },
+            { x: 4, y: 3 },
+            { x: 5, y: 4 },
+            { x: 6, y: 9 },
+            { x: 7, y: 7 },
+            { x: 8, y: 12 },
+          ],
+        },{
+          label: 'Data02',
+          backgroundColor: 'rgba(153, 255, 255, 0.5)',
+          borderColor: "rgba(255, 99, 132, 0)",
+          pointRadius: 0,
+          fill: true,
+          data: [
+            { x: 0, y: 1 },
+            { x: 1, y: 4 },
+            { x: 2, y: 8 },
+            { x: 3, y: 12 },
+            { x: 4, y: 1 },
+            { x: 5, y: 5 },
+            { x: 6, y: 2 },
+            { x: 7, y: 3 },
+            { x: 8, y: 1 },
+          ],
+        }]
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+      intersect: false,
+        },
+        title: {
+          display: true,
+          text: 'Angular & Chart.js'
+        },
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            scaleLabel: {
+              labelString: 'X',
+              display: true,
+            }
+          }],
+          yAxes: [{
+            type: 'linear',
+            scaleLabel: {
+              labelString: 'Y',
+              display: true
+            }
+          }]
+        }
+      }
+    });
   }
-  
 }
--- src/app/chart-test/chart-test.component.ts
+++ src/app/chart-test/chart-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-chart-test',
