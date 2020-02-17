d812eaef72d806baab8628283c4369819b4176f5
佐藤 駿郎 <toshiros1046@gmail.com>
2020-02-17 23:35:51 +0900

    bugcommit
    bugcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartTestComponent } from './chart-test/chart-test.component';
+import { ChartDetailComponent } from './chart-detail/chart-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartTestComponent
+    ChartTestComponent,
+    ChartDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chart-detail/chart-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-detail',
+  templateUrl: './chart-detail.component.html',
+  styleUrls: ['./chart-detail.component.css']
+})
+export class ChartDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
