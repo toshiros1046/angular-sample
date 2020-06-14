d9dc8ba84589c462bdb5178eed6fda984f1c4116
dhythm <Yuta.Okada.20@gmail.com>
2020-06-14 12:16:29 +0900

    fix/routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ChartDetailComponent } from "./chart-detail/chart-detail.component";
+import { ChartTestComponent } from "./chart-test/chart-test.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/test", pathMatch: "full" },
+  { path: "chart-detail", component: ChartDetailComponent },
+  { path: "test", component: ChartTestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
